809687249f1d2f53ea64383a5fd9e5a0f86f0bd1
luanlazz <luanlazzari@hotmail.com>
2021-01-27 12:41:47 -0300

    refactor: decompose checkout navigation from checkout

---
+++ src/components/checkout/checkout-navigation.tsx
@@ -0,0 +1,189 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Grid, Link, Typography } from '@material-ui/core'
+import newCard from '../../../assets/new-card.png'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import DoneIcon from '@material-ui/icons/Done'
+
+const getStepTitle = (step: number): React.ReactNode => {
+  switch (step) {
+    case 0:
+      return (
+        <>
+          <Icon>
+            <ShoppingCartIcon fontSize='large' />
+          </Icon>
+          <Title>
+            Confira seus produtos
+          </Title>
+        </>
+      )
+    case 1:
+      return (
+        <>
+          <NewCard src={newCard}/>
+          <Title>
+            Adicione um novo cartão de crédito
+          </Title>
+        </>
+      )
+    case 2:
+      return (
+        <>
+          <Icon>
+            <DoneIcon fontSize='large' />
+          </Icon>
+          <Title>
+            Revise o seu pedido e confirme
+          </Title>
+        </>)
+    default:
+      return (
+        <Title>
+          Etapa desconhecida
+        </Title>
+      )
+  }
+}
+
+const getPreviousStep = (step: number): string => {
+  switch (step) {
+    case 0:
+      return 'Adicionar outros produtos'
+    case 1:
+      return 'Alterar forma de pagamento'
+    case 2:
+      return 'Alterar informações de pagamento'
+    default:
+      return 'Etapa desconhecida'
+  }
+}
+
+interface Props {
+  activeStep: number
+  setActiveStep: Function
+  matchesMD: boolean
+  getSteps: Function
+}
+
+const CheckoutNavigation: React.FC<Props> = ({ activeStep, setActiveStep, matchesMD, getSteps }: Props) => {
+  const handleBack = (): void => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1)
+  }
+
+  return (
+    <Navigation item xs={12} sm={12} md={5}>
+      <Grid item>
+
+        {activeStep > 0 && matchesMD &&
+              <LinkComponent onClick={handleBack}>
+                <ArrowBackIosIcon />
+                {matchesMD && getPreviousStep(activeStep) }
+              </LinkComponent>
+        }
+
+        {!matchesMD &&
+              <Typography color='textSecondary'>
+                {activeStep > 0 &&
+                  <BackLink>
+                    <ArrowBackIosIcon onClick={handleBack} />
+                  </BackLink>
+                }
+                <b>{'Etapa '} {activeStep + 1}</b>
+                {' de '} {getSteps().length}
+              </Typography>
+        }
+      </Grid>
+
+      <StepTitle>
+        {getStepTitle(activeStep)}
+      </StepTitle>
+    </Navigation>
+  )
+}
+
+const Navigation = styled(Grid)`
+  background: ${({ theme }) => theme.palette.primary.main};
+  display: flex;
+  ${({ theme }) => theme.breakpoints.down('md')} {
+    justify-content: center;
+  }
+  ${({ theme }) => theme.breakpoints.up('md')} {
+    justify-content: flex-start;
+  }
+  margin: 0;
+  min-width: 330px;
+  position: relative;
+  ${({ theme }) => theme.breakpoints.down('md')} {
+    padding: ${({ theme }) => theme.spacing(3)}px;
+  }
+  ${({ theme }) => theme.breakpoints.up('md')} {
+    padding: ${({ theme }) => theme.spacing(5)}px
+             ${({ theme }) => theme.spacing(3)}px
+             ${({ theme }) => theme.spacing(3)}px
+             ${({ theme }) => theme.spacing(7)}px;
+  }   
+`
+
+const LinkComponent = styled(Link)`
+  align-items: center;
+  flex-direction: row;
+
+  && {
+    color: ${({ theme }) => theme.palette.text.secondary};
+    display: flex;
+    font-size: 22px;
+    text-decoration: none;
+    ${({ theme }) => theme.breakpoints.down('md')} {
+      justify-content: flex-start;
+    }
+
+    :hover {
+      text-decoration: none;
+      cursor: pointer;
+    }
+  }
+`
+
+const BackLink = styled.div`
+  position: absolute;
+  left: ${({ theme }) => theme.spacing(4)}px;
+  cursor: pointer;
+`
+
+const StepTitle = styled.div`
+  align-items: center;
+  color: ${({ theme }) => theme.palette.text.secondary};
+  display: flex;
+  flex-direction: row;  
+  max-width: 300px;
+  position: absolute;
+  ${({ theme }) => theme.breakpoints.down('md')} {
+    top: ${({ theme }) => theme.spacing(8)}px;
+  }
+  ${({ theme }) => theme.breakpoints.up('md')} {
+    top: ${({ theme }) => theme.spacing(14)}px;
+  }  
+`
+
+const Title = styled.h2` 
+  font-weight: bold;
+`
+
+const NewCard = styled.img`
+  height: 50px;
+  width: 50px;
+  padding-right: ${({ theme }) => theme.spacing(2)}px;
+`
+
+const Icon = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  height: 50px;
+  width: 50px;
+  padding-right: ${({ theme }) => theme.spacing(2)}px;
+`
+
+export default CheckoutNavigation
--- src/components/checkout/checkout.tsx
+++ src/components/checkout/checkout.tsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import newCard from '../../../assets/new-card.png'
-import { Button, Grid, Link, Paper, Step, StepLabel, Stepper, Typography, useMediaQuery, useTheme } from '@material-ui/core'
-import { breakpoints } from '@material-ui/system'
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+import { Button, Grid, Paper, Step, StepLabel, Stepper, Typography, useMediaQuery, useTheme } from '@material-ui/core'
+import CheckoutNavigation from './checkout-navigation'
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
-import DoneIcon from '@material-ui/icons/Done'
 
 const getSteps = (): any[] => {
   return [
@@ -14,47 +10,6 @@ const getSteps = (): any[] => {
     'Pagamento',
     'Confirmação'
   ]
-}
-
-const getStepTitle = (step: number): React.ReactNode => {
-  switch (step) {
-    case 0:
-      return (
-        <>
-          <Icon>
-            <ShoppingCartIcon fontSize='large' />
-          </Icon>
-          <Title>
-            Confira seus produtos
-          </Title>
-        </>
-      )
-    case 1:
-      return (
-        <>
-          <NewCard src={newCard}/>
-          <Title>
-            Adicione um novo cartão de crédito
-          </Title>
-        </>
-      )
-    case 2:
-      return (
-        <>
-          <Icon>
-            <DoneIcon fontSize='large' />
-          </Icon>
-          <Title>
-            Revise o seu pedido e confirme
-          </Title>
-        </>)
-    default:
-      return (
-        <Title>
-          Etapa desconhecida
-        </Title>
-      )
-  }
 }
 
 const getStepContent = (step: number): string => {
@@ -70,74 +25,38 @@ const getStepContent = (step: number): string => {
   }
 }
 
-const getPreviousStep = (step: number): string => {
-  switch (step) {
-    case 0:
-      return 'Adicionar outros produtos'
-    case 1:
-      return 'Alterar forma de pagamento'
-    case 2:
-      return 'Alterar informações de pagamento'
-    default:
-      return 'Etapa desconhecida'
-  }
-}
-
 const Checkout: React.FC = () => {
   const [activeStep, setActiveStep] = useState(0)
   const steps = getSteps()
   const theme = useTheme()
-  const matches = useMediaQuery(theme.breakpoints.up('md'))
+  const matchesMD = useMediaQuery(theme.breakpoints.up('md'))
 
   const handleNext = (): void => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1)
   }
 
-  const handleBack = (): void => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1)
-  }
-
   return (
-    <CheckoutContainer container xs={12}>
+    <CheckoutContainer container>
       <CheckoutPaper>
-        <Navigation item xs={12} sm={12} md={5}>
-          <Grid item>
-
-            {activeStep > 0 && matches &&
-              <LinkComponent onClick={handleBack}>
-                <ArrowBackIosIcon />
-                {matches && getPreviousStep(activeStep) }
-              </LinkComponent>
-            }
-
-            {!matches &&
-              <Typography color='textSecondary'>
-                {activeStep > 0 &&
-                  <BackLink>
-                    <ArrowBackIosIcon onClick={handleBack} />
-                  </BackLink>
-                }
-                <b>{'Etapa '} {activeStep + 1}</b>
-                {' de '} {getSteps().length}
-              </Typography>
-            }
-          </Grid>
-
-          <StepTitle>
-            {getStepTitle(activeStep)}
-          </StepTitle>
-        </Navigation>
+        <CheckoutNavigation
+          activeStep={activeStep}
+          setActiveStep={setActiveStep}
+          getSteps={getSteps}
+          matchesMD={matchesMD}
+        />
 
         <CheckoutStepper item xs={12} sm={12} md={7}>
-          {matches &&
+          {!!matchesMD &&
             <Stepper activeStep={activeStep} connector={<ArrowForwardIosIcon color='primary' />}>
-              {steps.map((label, index) => (
-                <Step key={index}>
-                  <StepLabel>
-                    {label}
-                  </StepLabel>
-                </Step>
-              ))}
+              {steps.map((label, index) => {
+                const stepProps = {}
+                const labelProps = {}
+                return (
+                  <Step key={label} {...stepProps}>
+                    <StepLabel {...labelProps}>{label}</StepLabel>
+                  </Step>
+                )
+              })}
             </Stepper>
           }
 
@@ -159,8 +78,8 @@ const Checkout: React.FC = () => {
                     variant="contained"
                     color="primary"
                     onClick={handleNext}
-                    fullWidth
                     size='large'
+                    fullWidth
                   >
                     {activeStep === steps.length - 1 ? 'Finalizar Pedido' : 'Continuar'}
                   </Button>
@@ -190,89 +109,6 @@ const CheckoutPaper = styled(Paper)`
   ${({ theme }) => theme.breakpoints.up('sm')} {
     margin: ${({ theme }) => theme.spacing(4)}px;
   }
-`
-
-const Navigation = styled(Grid)`
-  background: ${({ theme }) => theme.palette.primary.main};
-  display: flex;
-  ${({ theme }) => theme.breakpoints.down('md')} {
-    justify-content: center;
-  }
-  ${({ theme }) => theme.breakpoints.up('md')} {
-    justify-content: flex-start;
-  }
-  margin: 0;
-  min-width: 330px;
-  position: relative;
-  ${({ theme }) => theme.breakpoints.down('md')} {
-    padding: ${({ theme }) => theme.spacing(3)}px;
-  }
-  ${({ theme }) => theme.breakpoints.up('md')} {
-    padding: ${({ theme }) => theme.spacing(5)}px
-             ${({ theme }) => theme.spacing(3)}px
-             ${({ theme }) => theme.spacing(3)}px
-             ${({ theme }) => theme.spacing(7)}px;
-  }   
-`
-
-const LinkComponent = styled(Link)`
-  align-items: center;
-  flex-direction: row;
-
-  && {
-    color: ${({ theme }) => theme.palette.text.secondary};
-    display: flex;
-    font-size: 22px;
-    text-decoration: none;
-    ${({ theme }) => theme.breakpoints.down('md')} {
-      justify-content: flex-start;
-    }
-
-    :hover {
-      text-decoration: none;
-      cursor: pointer;
-    }
-  }
-`
-
-const BackLink = styled.div`
-  position: absolute;
-  left: ${({ theme }) => theme.spacing(4)}px;
-  cursor: pointer;
-`
-
-const StepTitle = styled.div`
-  align-items: center;
-  color: ${({ theme }) => theme.palette.text.secondary};
-  display: flex;
-  flex-direction: row;  
-  max-width: 300px;
-  position: absolute;
-  ${({ theme }) => theme.breakpoints.down('md')} {
-    top: ${({ theme }) => theme.spacing(8)}px;
-  }
-  ${({ theme }) => theme.breakpoints.up('md')} {
-    top: ${({ theme }) => theme.spacing(14)}px;
-  }  
-`
-
-const Title = styled.h2` 
-  font-weight: bold;
-`
-
-const NewCard = styled.img`
-  height: 50px;
-  width: 50px;
-  padding-right: ${({ theme }) => theme.spacing(2)}px;
-`
-
-const Icon = styled.div`
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  height: 50px;
-  width: 50px;
-  padding-right: ${({ theme }) => theme.spacing(2)}px;
 `
 
 const CheckoutStepper = styled(Grid)`
@@ -310,6 +146,10 @@ const ButtonNextStep = styled(Grid)`
   position: absolute;
   right: ${({ theme }) => theme.spacing(2)}px;
   width: 246px;
+
+  & .MuiButton-root {
+    border-radius: 10px;
+  }
 `
 
 export default Checkout
