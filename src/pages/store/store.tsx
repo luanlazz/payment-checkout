c51dadd63a08851c42d3c89c3c1ff7e4dacd335d
luanlazz <luanlazzari@hotmail.com>
2021-01-29 18:20:30 -0300

    refactor: rename component

--- src/pages/store/store.tsx
+++ src/pages/store/store.tsx
@@ -3,13 +3,13 @@ import styled from 'styled-components'
 import { Checkout, Header } from '@/components'
 
 const Store: React.FC = () => (
-  <Background>
+  <StoreContainer>
     <Header />
     <Checkout />
-  </Background>
+  </StoreContainer>
 )
 
-const Background = styled.div`
+const StoreContainer = styled.div`
   background: ${({ theme }) => theme.palette.background.default} 0% 0% no-repeat padding-box; 
   box-sizing: border-box;
   height: 100vh;
